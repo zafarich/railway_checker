cd926a001e70207878bd3ceba96d828d5ed61c77
zafarich <zqalandarov@texnomart.uz>
2025-04-22 10:39:54 +0500

    start.js faylida biletlarni tekshirish funksiyasi qo'shildi va biletlar uchun yangi sanalar va stansiyalar belgilandi. Tekshirish intervali 70 soniyaga o'zgartirildi.

--- commands/start.js
+++ commands/start.js
@@ -11,6 +11,10 @@ bot.command("start", async (ctx) => {
   let has_error = false;
 
   setInterval(async () => {
+    await check_bilet();
+  }, 70000);
+
+  async function check_bilet() {
     try {
       const result = await axios({
         method: "POST",
@@ -18,48 +22,42 @@ bot.command("start", async (ctx) => {
         data: {
           direction: [
             {
-              depDate: "31.12.2024",
+              depDate: "27.05.2025",
               fullday: true,
               type: "Forward",
             },
           ],
           stationFrom: "2900000",
-          stationTo: "2900885",
+          stationTo: "2900790",
           detailNumPlaces: 1,
           showWithoutPlaces: 0,
         },
 
         headers: {
-          "X-Xsrf-Token": "1eebe68f-c362-4f97-8c1e-3fe5fe12dfce",
+          "X-Xsrf-Token": "64f763e3-c5eb-44e0-a3fe-a75883aad2cc",
           Cookie:
-            "_ga=GA1.1.518240125.1709534066; __stripe_mid=8e26af5c-80ba-473c-a359-3395f017140340230d; G_ENABLED_IDPS=google; __stripe_sid=bbbc1f8a-7815-437d-af39-639ef7046058150520; XSRF-TOKEN=1eebe68f-c362-4f97-8c1e-3fe5fe12dfce; G_AUTHUSER_H=0; _ga_K4H2SZ7MWK=GS1.1.1712641623.9.1.1712643365.0.0.0",
+            "_ga=GA1.1.1452817594.1733381651; __stripe_mid=450fa355-8e90-46de-b0d1-da3e109c1b8fbf5719; G_ENABLED_IDPS=google; __stripe_sid=22bc924f-9dcb-414e-ab30-d88428a96268fc071a; XSRF-TOKEN=64f763e3-c5eb-44e0-a3fe-a75883aad2cc; _ga_K4H2SZ7MWK=GS1.1.1735271997.23.1.1735272499.0.0.0",
           "Device-Type": "BROWSER",
           "Accept-Language": "uz",
         },
       });
 
       const trains = result?.data?.express?.direction?.[0]?.trains?.[0]?.train;
+      let hasAvailableTickets = false;
+
       trains.forEach((train) => {
         const cars = train?.places?.cars || [];
 
-        const cars_pk = cars.filter((item) => item.typeShow === "Plaskartli");
+        const cars_pk = cars.filter(
+          (item) => item.typeShow === "Plaskartli" || item.typeShow === "Kupe"
+        );
         const cars_length = cars_pk?.length;
 
-        if (cars_length) {
+        if (cars_length && !hasAvailableTickets) {
+          hasAvailableTickets = true;
           ctx.reply("Bilet chiqdiiiiiiiiiii !!!!!");
         }
       });
-
-      // const result = await axios({
-      //   method: "GET",
-      //   url: "https://api.iticket.uz/uz/v5/events/search?client=web&q=uzbekistan%20vs",
-      // });
-
-      // const events = result.data?.response?.events;
-
-      // if (events?.length > 0) {
-      //   ctx.reply("Bilet chiqdiiiiiiiiiii !!!!!");
-      // }
     } catch (error) {}
-  }, 30000);
+  }
 });
